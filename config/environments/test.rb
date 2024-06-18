aaaee653d3dbef52ec337082351dd016d11aa17b
SAKURAchan916 <syouto1443@gmail.com>
2024-06-18 11:30:40 +0900

    各モデルのコミット

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
     "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
+  config.active_job.queue_adapter = :inline #追記
+  
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
